1c739800d49fee25e6ecdecac78ae10e551f3b4c
Ka-Ping Yee <ping@zesty.ca>
2013-03-25 22:48:22 -0700

    Add github link.

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from distutils.core import setup
-setup(name='q', version='2.1', py_modules=['q'],
+setup(name='q', version='2.2', py_modules=['q'],
       description='Quick-and-dirty debugging output',
       author='Ka-Ping Yee', author_email='ping@zesty.ca',
-      url='http://zesty.ca/python/', classifiers=[
+      url='http://github.com/zestyping/q', classifiers=[
           'Programming Language :: Python',
           'Intended Audience :: Developers',
           'License :: OSI Approved :: Apache Software License'
