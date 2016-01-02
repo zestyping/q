835f0f1097c0cd040d4bb1b25a9da44a7afe0d47
Federico Ceratto <federico.ceratto@gmail.com>
2016-01-02 11:46:45 +0000

    Add classifiers and keywords

--- setup.py
+++ setup.py
@@ -1,10 +1,21 @@
 from setuptools import setup
-setup(name='q', version='2.6', py_modules=['q'],
-      description='Quick-and-dirty debugging output for tired programmers',
-      author='Ka-Ping Yee', author_email='ping@zesty.ca',
-      license='Apache License 2.0',
-      url='http://github.com/zestyping/q', classifiers=[
-          'Programming Language :: Python',
-          'Intended Audience :: Developers',
-          'License :: OSI Approved :: Apache Software License'
-      ])
+setup(
+    name='q', version='2.6', py_modules=['q'],
+    description='Quick-and-dirty debugging output for tired programmers',
+    author='Ka-Ping Yee', author_email='ping@zesty.ca',
+    license='Apache License 2.0',
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Programming Language :: Python :: Implementation :: Jython",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+    ],
+    keywords=['debugging'],
+    url='http://github.com/zestyping/q'
+)
